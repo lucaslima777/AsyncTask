48e5f510ed1cc706ca3285775f262eba6dce0183
Lucas Lima T <lukaslymauchiha@gmail.com>
2020-02-24 12:04:01 -0300

    Initial commit

---
+++ app/src/main/java/asynctask/lln/AsyncTask/AsynckTaskAdvanced.java
@@ -0,0 +1,30 @@
+package asynctask.lln.AsyncTask;
+
+import android.os.AsyncTask;
+
+public class AsynckTaskAdvanced extends AsyncTask {
+
+    public AsynckTaskAdvanced(){
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+    }
+}
